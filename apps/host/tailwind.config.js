0abb9b9e4f6b9216ec7fdc8fdc2694d7e1fe1f7c
psychomet <psychomet.dev@gmail.com>
2024-06-23 13:52:13 +0330

    feat(host): setup tailwind

---
+++ apps/host/tailwind.config.js
@@ -0,0 +1,24 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '0rem',
+      },
+    },
+  },
+  plugins: [],
+};
