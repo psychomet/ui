1ab9093e49d761ca0c3d93aa886c19f1c6cb684f
psychomet <psychomet.dev@gmail.com>
2024-06-23 13:50:40 +0330

    feat(host): complete configure ng-zorro-antd

--- apps/host/src/app/app.config.ts
+++ apps/host/src/app/app.config.ts
@@ -1,18 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
-import { fa_IR, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 import fa from '@angular/common/locales/fa';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
+import { en_US, fa_IR, provideNzI18n } from 'ng-zorro-antd/i18n';
+
+import { appRoutes } from './app.routes';
 
 registerLocaleData(fa);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideNzI18n(fa_IR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(),
+    provideRouter(appRoutes),
+    provideNzI18n(fa_IR as typeof en_US),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    provideHttpClient(),
   ],
 };
