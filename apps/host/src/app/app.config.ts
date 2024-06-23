d32d7ac394fc0b9a237093bfedd7a7c3aab54238
psychomet <psychomet.dev@gmail.com>
2024-06-23 11:39:13 +0330

    feat(host): configure ng-zorro-antd

--- apps/host/src/app/app.config.ts
+++ apps/host/src/app/app.config.ts
@@ -1,10 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { fa_IR, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fa from '@angular/common/locales/fa';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(fa);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideNzI18n(fa_IR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient(),
   ],
 };
