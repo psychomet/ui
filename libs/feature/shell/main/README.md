e719cef68fee3cf4aa8aaf8a87ac5a2049ddaccf
psychomet <psychomet.dev@gmail.com>
2024-06-23 13:59:16 +0330

    feat(feature-shell-main): initial commit

---
+++ libs/feature/shell/main/README.md
@@ -0,0 +1,7 @@
+# feature-shell-main
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test feature-shell-main` to execute the unit tests.
---
+++ libs/feature/shell/main/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { MainComponent } from './main.component';
+
+export const mainRoutes: Route[] = [{ path: '', component: MainComponent }];
