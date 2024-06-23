37f3198b6fdf4e1797e3484e268b4069ab127b61
psychomet <psychomet.dev@gmail.com>
2024-06-23 15:03:10 +0330

    feat(shared-util): initial commit

---
+++ libs/shared/util/src/lib/util.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-util',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './util.component.html',
+  styleUrl: './util.component.less',
+})
+export class UtilComponent {}
