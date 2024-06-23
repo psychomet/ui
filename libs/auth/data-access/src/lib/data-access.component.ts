e234c8e96151944e801d0e2b12a85319f44f3299
psychomet <psychomet.dev@gmail.com>
2024-06-23 15:05:50 +0330

    feat(auth-data-access): initial commit

---
+++ libs/auth/data-access/src/lib/data-access.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-data-access',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data-access.component.html',
+  styleUrl: './data-access.component.less',
+})
+export class DataAccessComponent {}
